23271e28488a35bd6ad07a696743929ca34b6967
Jaehoon AN <mudblood.ev@gmail.com>
2023-11-01 14:21:37 +0900

    renv shapshot test

---
+++ R/ggplot_sample.R
@@ -0,0 +1,17 @@
+### github and R project
+
+# 1. make github repository
+# 2. setting git in local PC
+# 3. make new R project -> Version control -> Git -> Clone Git Repository
+
+library(renv)
+install("ggplot2@3.3.2")
+
+library(ggplot2)
+installed.packages()["ggplot2",]
+
+ggp <- ggplot() +
+  geom_boxplot(aes(x=factor(cyl), y=mpg, fill=factor(am)), mtcars) +
+  scale_x_discrete("cyl") + guides(fill=guide_legend("am")) +
+  theme_classic()
+ggp
